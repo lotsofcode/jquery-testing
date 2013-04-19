bd14ffaf9cebc07161c392ee085aacaedfe179ed
Del <del@lotsofcode.com>
2013-04-20 00:00:39 +0100

    Cleanup of initial tests

--- spec/jQuerySpec.js
+++ spec/jQuerySpec.js
@@ -1,10 +1,14 @@
 describe("jQuery", function() {
 
-	it ("should instantiate jQuery as $ object", function() {
+	it ("should be accessible through $", function() {
+		expect($).toEqual(jQuery)
+	});
+
+	it ("should instantiate $ as object", function() {
 		expect(typeof $).toBe('function')
 	});
 
-	it ("jQuery version should be 2.0.0", function() {
+	it ("version should be 2.0.0", function() {
 		expect($.fn.jquery).toBe('2.0.0')
 	})
 
