4e280d9c76f019c8688ea4f00e3a640ad1021104
Del <del@lotsofcode.com>
2013-04-21 22:20:06 +0100

    Updated specs to include element tests

--- spec/jQuerySpec.js
+++ spec/jQuerySpec.js
@@ -1,21 +1,50 @@
 describe("jQuery", function() {
 
 	it ("should be accessible through $", function() {
-		expect($).toEqual(jQuery)
+		expect($).toBe(jQuery);
 	});
 
 	it ("should instantiate $ as object", function() {
-		expect(typeof $).toBe('function')
+		expect(typeof $).toBe('function');
 	});
 
 	it ("version should be 2.0.0", function() {
-		expect($.fn.jquery).toBe('2.0.0')
+		expect($.fn.jquery).toBe('2.0.0');
 	})
 
 	describe("Selector", function() {
 		it ("should have length 0 if dom element doesn't exist", function() {
 			expect($('.missing-selector').length).toEqual(0);
 		})
+	});
+
+	describe("Element", function() {
+
+		var element;
+		beforeEach(function() {
+			element = $('<div id="foobar" class="foo bar">');
+		});
+
+		it ("should allow access to id from attr() method", function() {
+			expect(element.attr('id')).toBe('foobar');
+		})
+
+		it ("should return attr() undefined after removal of attribute", function() {
+			element.removeAttr('id');
+			expect(element.attr('id')).toBeUndefined();
+		})
+
+		it ("should increase children length when using append()", function() {
+			element.append("<div>");
+			expect(element.children().length).toEqual(1);
+		})
+
+		it ("should retain attributes when using clone()", function() {
+			var cloned = element.clone();
+			expect(cloned.attr('id')).toBe('foobar');
+			expect(cloned.attr('class')).toBe('foo bar');
+		})
+
 	});
 
 })
